c301e06b4cfcd4f251607d9c7b61774bb19ad5d9
rgupta <rgupta@travelrepublic.com>
2020-06-29 19:06:38 +0100

    Updates

---
+++ precache-manifest.34371da1be4b20a422f18610c1300293.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8922646baa32f0b55d996f45dfa4aa98",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "2969a54e917b7dd95286",
+    "url": "/TicTacToe/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "dbdf6094b99a074ddd8a",
+    "url": "/TicTacToe/static/js/2.094fd168.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe/static/js/2.094fd168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2969a54e917b7dd95286",
+    "url": "/TicTacToe/static/js/main.8fd75929.chunk.js"
+  },
+  {
+    "revision": "35d19f0c13e4574912e8",
+    "url": "/TicTacToe/static/js/runtime-main.29f4516c.js"
+  }
+]);
